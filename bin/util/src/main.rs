f79e75a983c2ecce7ae6d11140e4ab78fca775ed
Svyatoslav Nikolsky <svyatonik@gmail.com>
2020-02-20 11:36:08 +0300

    fix warning

--- bin/util/src/main.rs
+++ bin/util/src/main.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity Ethereum.  If not, see <http://www.gnu.org/licenses/>.
 
-use parity_crypto::publickey::{Secret, Signature, Message, sign};
+use parity_crypto::publickey::{Secret, Message, sign};
 
 fn main() {
 	run().unwrap();
